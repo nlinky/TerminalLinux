08c90ec21d6b0651c4ef4a1e8096976e1adff814
nadya.chervyakova <kraynova06@yandex.ru>
2021-10-08 08:56:46 +0300

    linux

---
+++ dz_Linux2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd Linux_commands/
+mkdir one_papka two_papka three_papka
+cd one_papka/
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir one_one one_two one_three
+ls -la
+mv C:/Users/Cherv/git/course/Linux_commands/one_papka/file4.json C:/Users/Cherv/git/course/Linux_commands/one_papka/file5.json C:/Users/Cherv/git/course/Linux_commands/two_papka
